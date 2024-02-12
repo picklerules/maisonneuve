1dbd67298bf375a0ba9939c843acf4ca327bd4aa
picklerules <136508588+picklerules@users.noreply.github.com>
2024-02-12 12:47:27 -0500

    ajout de layouts.app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name')}} - @yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/0f52bb4695.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">{{ config('app.name')}}</a>
+            <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-house fa-lg" style="color: #2c6ddd;"></i></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+
+                    <li class="nav-item">
+                    <a class="nav-link text-primary" href="#">Étudiants</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-primary" href="#">Villes</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </header>
+    <div class="container">
+        @yield('content')
+    </div>
+    <footer class="bg-light text-center">
+        <div class="p-3">
+            &copy; PickleRules {{ date('Y') }} {{ config('app.name') }}
+        </div>
+    </footer>
+
+</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</html>
